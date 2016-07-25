5701ad71eca9bc501d0cdb048ff68b7038d5f0c0
Игорь Михайлович Ракитянский <i697@ya.ru>
2016-07-25 23:05:03 +0600

    Извлечение данных из data.plist(xml)

--- MyCars/ViewController.swift
+++ MyCars/ViewController.swift
@@ -25,6 +25,51 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     
     }
+
+    func insertDataFromPlist() {
+        let fetchRequest = NSFetchRequest(entityName: "Car")
+        fetchRequest.predicate = NSPredicate(format: "mark != nil")
+
+        let count = managedContext.countForFetchRequest( fetchRequest, error: nil)
+
+        guard count == 0 else {return}
+
+        let pathToFile = NSBundle.mainBundle().pathForResource("data", ofType: "plist")
+        let daraArray = NSArray(contentsOfFile: pathToFile!)
+
+        for dictionary: AnyObject in daraArray! {
+            let entity = NSEntityDescription.entityForName("Car", inManagedObjectContext: managedContext)
+            let car = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext) as! Car
+
+            let carDict = dictionary as! NSDictionary
+
+            car.mark = carDict["mark"] as! String
+            car.model = carDict["model"] as! String
+            car.rating = carDict["rating"] as! NSNumber
+
+            let colorDict = carDict["tintcolor"] as! NSDictionary
+            car.tintColor = getColor(colorDict)
+
+            let imageName = carDict["imageName"] as! String
+            let image = UIImage(named: imageName)
+            let imageData = UIImagePNGRepresentation(image!)
+            car.imageData = imageData
+
+            car.lastStarted = carDict["lastStarted"] as! NSDate
+            car.timesDriven = carDict["timesDriven"] as! NSNumber
+            car.myChoice = carDict["myChoice"] as! NSNumber
+
+
+        }
+    }
+
+    func getColor(dict: NSDictionary) -> UIColor {
+        let red = dict["red"] as! NSNumber
+        let blue = dict["blue"] as! NSNumber
+        let green = dict["green"] as! NSNumber
+
+        return UIColor(red: CGFloat(red) / 255, green: CGFloat(blue) / 255, blue: CGFloat(green) / 255, alpha: 1.0)
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
